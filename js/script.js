df9826fb48f7dfb309e7bb0824594919acec7739
Brieanna Wilson <brieanna.wilson@salem.edu>
2025-10-30 13:20:01 -0400

    Update project files (.gitignore, index.html, script.js)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 console.log("script.js loaded");
+endpoint = "https://api.giphy.com/v1/gifs/search?api_key=Wwwy4MklFdocdx2UpaTXqr4ZaH3PChlD&q=Cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+fetch(endpoint)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const container = document.getElementById("gif-container");
+    data.data.forEach((gif) => {
+      const img = document.createElement("img");
+      img.src = gif.images.fixed_height.url;
+      container.appendChild(img);
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching GIFs:", error);
+  }); 
+  gif-container = document.querySelector('#gif-container');
+
+  fetch-gifs = document.querySelector('#fetch-gifs');
+
+  console.log(fetch-gifs);
+  console.log(gif-container);
